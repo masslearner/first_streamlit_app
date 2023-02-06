115525bd8f6addb044fc53797d68871fed414f78
masslearner <124604187+masslearner@users.noreply.github.com>
2023-02-06 13:15:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import emoji
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(:stew: 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(:kale: 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(:chicken: 'Hard-Boiled Free-Range Egg')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥬 'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
