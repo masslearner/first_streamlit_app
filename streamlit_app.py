e8f4e93320584f617f8ceaf72db47c1850357ae7
masslearner <124604187+masslearner@users.noreply.github.com>
2023-02-06 13:07:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(':ramen: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':kale: Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
